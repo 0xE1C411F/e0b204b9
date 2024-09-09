1d3d0c268d7ccd468e01598e5f862c7de62d5b27
0xE1C411F <0xE1C411F@proton.me>
2024-09-09 22:41:55 +0100

    Make helpers header-only

--- tests/helpers/test_session.h
+++ tests/helpers/test_session.h
@@ -1,7 +1,29 @@
+#ifndef _TEST_HELPERS_SESSION_H_
+#define _TEST_HELPERS_SESSION_H_
+
 #include <functional>
+#include <iostream>
 #include <vector>
 
 namespace helpers {
-int run_test(std::function<bool(void)> testing_function);
-int run_test_session(std::vector<std::function<bool(void)>> unit_tests);
+inline int run_test(std::function<bool(void)> testing_function) {
+  bool outcome = testing_function();
+  if (outcome) {
+    std::cout << '.' << std::flush;
+  } else {
+    std::cout << 'E' << std::flush;
+  }
+  return (int)!outcome;
+}
+
+inline int run_test_session(std::vector<std::function<bool(void)>> unit_tests) {
+  int error_counter = 0;
+  for (auto unit_test : unit_tests) {
+    run_test(unit_test);
+  }
+  std::cout << ' ' << std::flush;
+  return error_counter;
+}
 }  // namespace helpers
+
+#endif
