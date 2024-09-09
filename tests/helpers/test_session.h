00ba8bad9b5d569c20cb87c67fd3227a47600094
0xE1C411F <0xE1C411F@proton.me>
2024-09-09 23:53:27 +0100

    Fix test helpers

--- tests/helpers/test_session.h
+++ tests/helpers/test_session.h
@@ -19,7 +19,7 @@ inline int run_test(std::function<bool(void)> testing_function) {
 inline int run_test_session(std::vector<std::function<bool(void)>> unit_tests) {
   int error_counter = 0;
   for (auto unit_test : unit_tests) {
-    run_test(unit_test);
+    error_counter += run_test(unit_test);
   }
   std::cout << ' ' << std::flush;
   return error_counter;
