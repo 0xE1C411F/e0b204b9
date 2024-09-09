c71f9c7635024360c268cdffe785e060315425f7
0xE1C411F <0xE1C411F@proton.me>
2024-09-09 22:41:27 +0100

    Add error message

--- src/core/exceptions.h
+++ src/core/exceptions.h
@@ -2,14 +2,15 @@
 #define CORE_EXCEPTIONS_H_
 
 #include <stdexcept>
+#include <string>
 
 namespace core {
 /**
  * Check condition and throw exception if not satisfied
  */
-inline void require(bool condition) {
+inline void require(bool condition, const std::string& error_message = "required condition not satisfied") {
   if (!condition) {
-    throw std::invalid_argument("required condition not satisfied");
+    throw std::invalid_argument(error_message);
   }
 }
 }  // namespace core
