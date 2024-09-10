718a34a27943ed090c259877a90864b026c26f98
0xE1C411F <0xE1C411F@proton.me>
2024-09-10 19:10:11 +0100

    Try to fix github logs

--- tests/helpers/test_session.h
+++ tests/helpers/test_session.h
@@ -12,13 +12,13 @@
 
 namespace helpers {
 inline int run_test_session(std::vector<std::tuple<std::function<bool(void)>, const char*>> unit_tests) {
-  std::cout << "\033[34;1m━┯━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+  std::cout << "\033[94m━┯━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
             << std::endl;
   int error_counter = 0;
   int number_of_tests = unit_tests.size();
   for (int test_number = 0; test_number < number_of_tests; test_number++) {
     const auto& [function, name] = unit_tests[test_number];
-    std::cout << " \033[34;1m" << (test_number < (number_of_tests - 1) ? "├" : "└") << "\033[0m " << name
+    std::cout << " \033[94m" << (test_number < (number_of_tests - 1) ? "├" : "└") << "\033[0m " << name
               << std::string(70 - strlen(name), ' ') << std::flush;
     if (!function()) {
       std::cout << "\033[31;1mFAILED\033[0m";
